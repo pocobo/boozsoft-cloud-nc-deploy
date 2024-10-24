352a932965ff6abedec3297760ef6305a1baa23a
cheng990629 <40415260+gaocheng001@users.noreply.github.com>
2024-10-24 22:56:04 +0800

    wip(初始化项目): 初始化项目

--- oceanbase-data-import/sqls/ncloud-nacos/data/ncloud-nacos/TABLE/config_info.sql
+++ oceanbase-data-import/sqls/ncloud-nacos/data/ncloud-nacos/TABLE/config_info.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b1443d7c9e993c8e998a056361fb873c1849f4d3f39de294d474fcc162e571a
-size 125177
+oid sha256:f67e1faa05f44dd4516a09664dcc3e5f87e4550f5499da73a3c782282183baf8
+size 125163
