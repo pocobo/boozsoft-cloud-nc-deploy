2e4a3b12563ebec2ed46556c548753bdff92d53f
cheng990629 <40415260+gaocheng001@users.noreply.github.com>
2024-10-25 10:44:18 +0800

    wip(初始化项目): 初始化项目

---
+++ run.sh
@@ -0,0 +1,9 @@
+# 1. 下载并解压
+wget https://github.com/pocobo/boozsoft-cloud-nc-deploy/archive/refs/tags/2.0.0.tar.gz && \
+tar -zxf 2.0.0.tar.gz && \
+cd boozsoft-cloud-nc-deploy-2.0.0
+
+# 2. 设置脚本权限并执行
+chmod +x ./app_install.sh && \
+./app_install.sh
+
